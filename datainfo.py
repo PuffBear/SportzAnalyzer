8a52c2bb7ffa2d87526e8f668ce5a0c307732ae5
Your Name <youremail@example.com>
2024-05-17 11:05:20 +0530

    Initial commit

---
+++ datainfo.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("atp_matches_2023.csv")
+
+print(df.head())
+print(df.tail())
+
+print(df.columns)
+df = df.dropna()
+
+country_heights = df.groupby('player1_ioc')['player1_ht'].mean().reset_index()
+country_heights.sort_values('player1_ht', inplace=True)  # Sorting helps in better visualization
+
+# Reshape the data for heatmap (if necessary)
+country_heights_pivot = country_heights.pivot("player1_ioc", "player1_ht", "player1_ht")
+
+plt.figure(figsize=(12, 8))  # Set the figure size as needed
+heatmap = sns.heatmap(country_heights_pivot, annot=True, cmap='coolwarm', fmt=".1f")
+heatmap.set_title('Average Player Heights by Country')
+plt.xlabel('Average Height')
+plt.ylabel('Country')
+plt.show()
